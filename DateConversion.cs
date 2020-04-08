07665f8368289f6a2c6d24634aeaec81e842048c
Anurag Rai <anurag8186@gmail.com>
2020-04-08 18:02:20 +0530

    Add files via upload
    ADO utility and date conversion

---
+++ DateConversion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+//using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+/// Summary description for DateConversion
+/// </summary>
+public class DateConversion
+{
+    public string getDateString(string date)
+    {
+        if (!string.IsNullOrEmpty(date))
+        {
+            string date1 = "";
+            //DateTime Date = DateTime.ParseExact(date, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            DateTime Date = DateTime.Parse(date, System.Globalization.CultureInfo.InvariantCulture);
+            date1 = Date.ToString("dd/MM/yyyy");
+
+
+            string a = date1.Substring(0, 10);
+            if (a == "01-01-2000")
+            {
+                a = "";
+                return a;
+            }
+            else
+            {
+                return a;
+            }
+
+        }
+        return "";
+    }
+
+    public string ChkDate(string date)
+    {
+        string date1 = "";
+        DateTime Date;
+        if (string.IsNullOrEmpty(date))
+        {
+            date1 = "01/01/1900";
+        }
+        else
+        {
+            string[] a = new string[6];
+            a = date.Split('-');
+            int l = a.Length;
+            if (l > 1)
+            {
+                Date = DateTime.ParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                date1 = Date.ToString("MM/dd/yyyy");
+            }
+            else
+            {
+                Date = DateTime.ParseExact(date, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                date1 = Date.ToString("MM/dd/yyyy");
+            }
+        }
+
+
+        return date1;
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+//using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Globalization;
+
+/// <summary>
+/// Summary description for Utility
+/// </summary>
+/// 
+
+namespace DataUtility
+{    
+    public class Utility
+    {
+        string dbconnection = System.Configuration.ConfigurationSettings.AppSettings["ConnectionString"];
+        public DateTime FormateDate(Object obj)
+        {
+            IFormatProvider culture = new CultureInfo("fr-FR", true);
+            if (obj == null || Convert.IsDBNull(obj) || obj.ToString().Trim() == string.Empty)
+                return GetMinDateTime();
+            else
+                return DateTime.ParseExact(obj.ToString(), "dd/MM/yyyy", culture);
+        }
+
+        private DateTime GetMinDateTime()
+        {
+            return DateTime.Parse("01/jan/1900");
+        }
+
+        public DataTable GetDataTable(string strQuery)
+        {            
+            SqlConnection conn = new SqlConnection(dbconnection);
+            SqlDataAdapter sda = new SqlDataAdapter(strQuery, conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            return dt;
+        }
+    }
+}
